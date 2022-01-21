1ba673acc80dfcdd046ec9efaf709f096842a3d8
sbstndbs <sebastien.dubois4@gmail.com>
2022-01-21 08:18:45 +0100

    Modify to one iteration
    Im trying to avoid dead code deletion without success...

--- functions/pow.c
+++ functions/pow.c
@@ -15,28 +15,29 @@ unsigned long long rdtsc(void)
 int main(){
 
 
-	unsigned long long n = 10000 ;
+	unsigned long long n = 1 ;
 	unsigned long long before, after ;
 	double tics = 0.0 ;  
 
-	float base = 100.0000 ; 
-	float r1 = 0.0 ; 
-	float r2 = 0.0 ; 
-	float r3 = 0.0 ; 
-	float r4 = 0.0 ; 
+	float base1 = 100.0000 ; 
+	float base2 = 100.0000 ; 	
+	float base3 = 100.0000 ; 	
+	float base4 = 100.0000 ; 	
+	
+	register float volatile r1 = 0.0 ; 
+	register float volatile r2 = 0.0 ; 
+	register float volatile r3 = 0.0 ; 
+	register float volatile r4 = 0.0 ; 
 	before = rdtsc();	
 	for (unsigned long long i = 0 ; i < n ; i++){
-		r1 = sqrt(base) ; 
-		r2 = sqrt(base) ; 
-		r3 = sqrt(base) ; 
-		r4 = sqrt(base) ; 
-		r1 = sqrt(base) ; 
-		r2 = sqrt(base) ; 
-		r3 = sqrt(base) ; 
-		r4 = sqrt(base) ; 
+		r1 = sqrt(base1) ; 
+		r2 = sqrt(base2) ; 
+		r3 = sqrt(base3) ; 
+		r4 = sqrt(base4) ; 
+
 	}
 	after = rdtsc();
-	tics = ((double)after - (double)before)/(8 * (double)n) ; 
+	tics = ((double)after - (double)before)/(4 * (double)n) ; 
 	
 	
 	printf("result : %f, %f, %f, %f \n", r1, r2, r3, r4);	
